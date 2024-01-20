ce9bafe5e6eda0633d3195475ddc945f4ccfcba6
krsison1 <156979962+krsison1@users.noreply.github.com>
2024-01-21 01:40:34 +0800

    Delete git directory

--- git/GroceryListEntry.py
+++
@@ -1,7 +0,0 @@
-class GroceryListEntry:
-    def __init__(self, item, qty, category, urgency, status):
-        self.item = item
-        self.qty = qty
-        self.category = category
-        self.urgency = urgency
-        self.status = status
